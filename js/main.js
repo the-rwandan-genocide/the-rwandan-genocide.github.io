f3202921d84d6f022b6b7777eaada1f724e58a1e
Monlih <MonliH@users.noreply.github.com>
2020-01-27 07:10:34 -0500

    Added refrences

--- js/main.js
+++ js/main.js
@@ -66,3 +66,40 @@ function animateValue(id, start, end, duration) {
   timer = setInterval(run, stepTime);
   run();
 }
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal)
+  })
+});
+
+overlay.addEventListener('click', function () {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+});
+
+closeModalButtons.forEach(function(button) {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal)
+  })
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active')
+}
